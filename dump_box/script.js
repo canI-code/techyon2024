444e8507c23f9344d464442acc165faffb51c47f
Nikhil Kumar <list.append17@gmail.com>
2024-09-01 21:18:21 +0530

    Added website files

---
+++ dump_box/script.js
@@ -0,0 +1,20 @@
+// // get both pupils
+// const pupils = document.querySelectorAll(".eye .pupil");
+// window.addEventListener("mousemove", (e) => {
+//   pupils.forEach((pupil) => {
+//     // get x and y postion of cursor
+//     var rect = pupil.getBoundingClientRect();
+//     var x = (e.pageX - rect.left) / 30 + "px";
+//     var y = (e.pageY - rect.top) / 30 + "px";
+//     pupil.style.transform = "translate3d(" + x + "," + y + ", 0px)";
+//   });
+// });
+
+
+
+$(function() {
+	$("#card").draggable();
+	$(".block").on("click", function() {
+		$(".info").toggleClass("active");
+	});
+});
